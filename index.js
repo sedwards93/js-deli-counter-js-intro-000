be6d40f1ad042aec10af533e42c0ddf9ce1ddae2
Symion Edwards <symion.edwards@gmail.com>
2020-04-16 09:22:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
 }
+
+function currentLine (line) {
+  if (katzDeliLine.length > 0) return ``;
+  else return "The line is currently empty"
+}
